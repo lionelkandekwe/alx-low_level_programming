90f557243660216f0229772b88538201fa98a4c8
lionelkandekwe <liokandekwe@gmail.com>
2022-12-09 07:48:04 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 x = n % 10;
-printf("Last digit of %d is %d", n, x);
+printf("Last digit of %d is %d ", n, x);
 if (x > 5)
 {
 printf("and is greater than 5");
@@ -27,5 +27,6 @@ else if (x < 6 && x != 0)
 {
 printf("and is less than 6 and not 0");
 }
+printf("\n");
 return (0);
 }
