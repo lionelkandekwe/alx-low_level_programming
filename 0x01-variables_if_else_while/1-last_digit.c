a9405d9ffe64a5a6ba4616d7d25ab80ebbc30d5d
lionelkandekwe <liokandekwe@gmail.com>
2022-12-09 06:45:28 -0800

    check if number is positive or negative

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print last digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+return (0);
+}
