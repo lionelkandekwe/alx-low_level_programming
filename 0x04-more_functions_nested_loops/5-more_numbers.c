89960aaa9b48337f264b1f07118f66f30285ce1c
lionelkandekwe <liokandekwe@gmail.com>
2022-12-15 01:44:37 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		if (j >= 10)
+		_putchar('1');
+		_putchar(j % 10 + '0');
+	}
+		_putchar('\n');
+	}
+}
